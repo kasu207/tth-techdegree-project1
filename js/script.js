4f2b4272d7718b8c11da2131bbf7649341e151a1
Phlpp <phlpp.rchtr@protonmail.com>
2020-04-20 22:21:25 +0200

    Beginning of Project
    quotes array,
    to late for more

--- js/script.js
+++ js/script.js
@@ -10,13 +10,60 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  {
+    quote: 'A wonderful fact to reflect upon, that every human creature is constituted to be that profound secret and mystery to every other.',
+    source: 'Charles Dickens',
+    citation:'A tale of Two Cities',
+    year: 1859
+  },
+  {
+    quote: 'According to Madam Pomfrey, thoughts could leave deeper scars than almost anything else.',
+    source: 'J.K. Rowling',
+    citation:'Harry Potter and the Order of the Phoenix',
+    year: 2003
+  },
+  {
+    quote: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live',
+    source: 'John Woods',
+    citation:'',
+    year:''
+  },
+  {
+    quote: "You've baked a really lovely cake, but then you've used dog shit for frosting.",
+    source: 'Steve Jobs',
+    citation: '',
+    year:''
+  },
+  {quote: 'Object-oriented programming offers a sustainable way to write spaghetti code. It lets you accrete programs as a series of patches.',
+    source: 'Paul Graham',
+    citation: ' Hackers & Painters: Big Ideas from the Computer Age',
+    year: 2004
+  },
+  {quote: 'If you ever find yourself in the wrong story, leave.',
+    source: 'Mo Willems',
+    citation: 'Goldilocks and the Three Dinosaurs',
+    year:2012
+  },
+  {
+    quote: 'Our wounds are often the openings into the best and most beautiful part of us.',
+    source: 'David Richo',
+    citation:'',
+    year:''
+  }
 
+];
 
+//console.log(quotes);
 
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote(){
+  let rand;
+  
 
+}
 
 
 /***
